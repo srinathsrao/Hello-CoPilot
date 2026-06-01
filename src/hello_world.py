f29458e47e5316c242c4161a7bb5f55617c4d84f
srinathsrao-rayze <srinath@rayze.xyz>
2026-06-01 20:04:52 +0530

    Adding Pythong code generated

---
+++ src/hello_world.py
@@ -0,0 +1,12 @@
+import argparse
+
+
+def greet(name: str) -> None:
+    print(f"Hello {name}")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("name", help="Name to greet")
+    args = parser.parse_args()
+    greet(args.name)
